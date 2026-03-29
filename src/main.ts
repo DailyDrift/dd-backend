cc668df40d2369eb11ffdc290decf6d8d162796c
L0rdlung3r <mail.an.yanik@gmail.com>
2026-03-29 12:32:45 +0200

    bugfixing

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
